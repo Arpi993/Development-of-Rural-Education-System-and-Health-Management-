95648f1b6b0d72cabd9517114db632ef65b327ee
Arpi993 <104126957+Arpi993@users.noreply.github.com>
2023-04-13 17:23:57 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,22 @@
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let formSection = document.querySelector(".form-section");
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+ 
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
+
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
